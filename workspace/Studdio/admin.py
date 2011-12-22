15feea2ba5729f0ee365cfa809a9212d992d6273
Derek Nelson <dereknelson01@gmail.com>
2011-12-22 14:07:30 -0500

    modifications and removed Studdio

--- workspace/Studdio/admin.py
+++
@@ -1,6 +0,0 @@
-from studdio.models import DataSet, DataTerm, UserProfile
-from django.contrib import admin
-
-admin.site.register(DataSet)
-admin.site.register(DataTerm)
-admin.site.register(UserProfile)
