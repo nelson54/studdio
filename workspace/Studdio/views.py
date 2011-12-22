302f38fd5cc093c40d14b3ace5952977880e6286
unknown <Alyssa@Alyssa-PC.(none)>
2011-12-21 19:58:21 -0800

    add models and views

--- workspace/Studdio/views.py
+++ workspace/Studdio/views.py
@@ -1,1 +1,56 @@
-# Create your views here.
+from django.http import HttpResponse, Http404
+from django.utils import simplejson as json
+from django.template.response import TemplateResponse
+from django.contrib.auth.models import User
+from studdio.models import DataSet, DataTerm, UserProfile
+
+def index(request):
+    return TemplateResponse(request, 'index.html')
+
+def user(request, user):
+    try:
+        user = User.objects.filter(username=user)[0]
+        user_profile = UserProfile.objects.get(pk=user.id)
+        user_json = json.dumps( user_profile.get_public_fields(), indent=2)
+    except UserProfile.DoesNotExist:
+        raise Http404
+    return HttpResponse(user_json, mimetype="application/json")
+
+def user_datasets(request, user):
+    try:
+        user = User.objects.filter(username=user)[0]
+        user_profile = UserProfile.objects.get(pk=user.id)
+        set_json = json.dumps( user_profile.get_public_dataSets(), indent=2)
+    except User.DoesNotExist:
+        raise Http404
+    return HttpResponse(set_json, mimetype="application/json")
+
+def user_datasets_set(request, user, dataset):
+    try:
+        user = User.objects.filter(username=user)[0]
+        #user_profile = UserProfile.objects.get(pk=user.id)
+        set_json = DataSet.objects.get(pk=int(dataset))
+        set_content_json = json.dumps(set_json.get_public_fields() , indent=2)
+    except User.DoesNotExist:
+        raise Http404
+    return HttpResponse(set_content_json, mimetype="application/json")
+
+def user_datasets_set_terms(request, user, dataset):
+    try:
+        user = User.objects.filter(username=user)[0]
+        #user_profile = UserProfile.objects.get(pk=user.id)
+        set_json = DataSet.objects.get(pk=int(dataset))
+        set_content_json = json.dumps(set_json.get_public_terms() , indent=2)
+    except User.DoesNotExist:
+        raise Http404
+    return HttpResponse(set_content_json, mimetype="application/json")
+
+def user_datasets_set_term(request, user, dataset, term):
+    try:
+        user = User.objects.filter(username=user)[0]
+        #user_profile = UserProfile.objects.get(pk=user.id)
+        term = DataTerm.objects.get(pk=int(term))
+        term_json = json.dumps(term.get_public_fields() , indent=2)
+    except User.DoesNotExist:
+        raise Http404
+    return HttpResponse(term_json, mimetype="application/json")
